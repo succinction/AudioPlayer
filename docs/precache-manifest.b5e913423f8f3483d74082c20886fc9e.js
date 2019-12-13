b53bec29fddba313560414f752915a9a600c6fc7
unknown <succinction@gmail.com>
2019-12-13 14:04:30 -0800

    add docs build

---
+++ docs/precache-manifest.b5e913423f8f3483d74082c20886fc9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec086987bcbfde6be64ceda9f1a0e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8daaf14b565d43fa232",
+    "url": "/static/css/main.bcdb9e83.chunk.css"
+  },
+  {
+    "revision": "b1424cd3e52f086b6419",
+    "url": "/static/js/2.9f57773a.chunk.js"
+  },
+  {
+    "revision": "f8daaf14b565d43fa232",
+    "url": "/static/js/main.36388453.chunk.js"
+  },
+  {
+    "revision": "3f41216a8367fe501106",
+    "url": "/static/js/runtime-main.08dda0ae.js"
+  }
+]);
